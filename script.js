ef1aad414a3c2bbf426f366e6d84602a7a4ac18a
notesandedges <stradtrek@gmail.com>
2016-09-18 13:37:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ node.append('circle')
     .attr('cx', function(d){return d.x;})
     .attr('cy', function(d){return d.y;})
     .attr('r', circleWidth)
-    .attr('fill', '#800080')
+    .attr('fill', '#663366')
 
 
 
