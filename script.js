d79064a69c999b8cb9638f0736856bb66c4b0a14
notesandedges <stradtrek@gmail.com>
2017-06-23 23:48:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,8 @@ var nodes = [
     {name: "March 2017", target:[3,4,5,6,7,8,11],href: "posts/march2017.html", width: 7},//9
     {name: "April 2017", target:[3],href: "posts/april2017.html", width: 5},//10
     {name: "Am I Insane?", target:[1,3,4,5,7,8,9],href: "posts/amiinsane.html", width: 7},//11
-    {name: "The 47 ronin and ZDT", target: [3,4,6], href: "posts/fortysevenronin.html", width: 6}
+    {name: "The 47 ronin and ZDT", target: [3,4,6], href: "posts/fortysevenronin.html", width: 6},
+    {name: "June 2017", target:[2,5,12],href: "posts/june2017.html", width: 5}
 ]
 var links = [];
 for(var i=0; i<nodes.length; i++){
