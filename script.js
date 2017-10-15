5f921d177ee5bdd862868de3faa2c9e59af3e2e3
notesandedges <stradtrek@gmail.com>
2017-10-14 21:40:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,12 @@ var nodes = [
     {name: "Week 3", target:[5,7,12],href: "http://nakatani-ries.rice.edu/student-profiles/2017-2/2017-u-s-fellows/2017-shivani-shukla/#Shukla_Week03", width: 5},//14
     {name: "Week 4", target:[5,7,12],href: "http://nakatani-ries.rice.edu/student-profiles/2017-2/2017-u-s-fellows/2017-shivani-shukla/#Shukla_Week04", width: 5},//14
     {name: "Week 5", target:[5,7,12],href: "http://nakatani-ries.rice.edu/student-profiles/2017-2/2017-u-s-fellows/2017-shivani-shukla/#Shukla_Week05", width: 5}//14
-    {name: "RoboCup 2017", target:[5,3,4,1,7],href: "posts/robocup17.html", width: 8}
+    {name: "RoboCup 2017", target:[5,3,4,1,7],href: "posts/robocup17.html", width: 8},
+    {name: "Week 6", target:[1,7,12],href: "http://nakatani-ries.rice.edu/student-profiles/2017-2/2017-u-s-fellows/2017-shivani-shukla/#Shukla_Week06", width: 5}//14
+    {name: "Week 7", target:[5,7,9],href: "http://nakatani-ries.rice.edu/student-profiles/2017-2/2017-u-s-fellows/2017-shivani-shukla/#Shukla_Week07", width: 5}//14
+    {name: "Week 8", target:[5,7,6],href: "http://nakatani-ries.rice.edu/student-profiles/2017-2/2017-u-s-fellows/2017-shivani-shukla/#Shukla_Week08", width: 5}//14
+    {name: "Week 9", target:[5,3,9],href: "http://nakatani-ries.rice.edu/student-profiles/2017-2/2017-u-s-fellows/2017-shivani-shukla/#Shukla_Week09", width: 5}//14
+    {name: "Week 10", target:[5,7,2],href: "http://nakatani-ries.rice.edu/student-profiles/2017-2/2017-u-s-fellows/2017-shivani-shukla/#Shukla_Week10", width: 5}//14
 ]
 var links = [];
 for(var i=0; i<nodes.length; i++){
